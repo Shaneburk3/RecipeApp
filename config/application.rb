1dc5005b5f8443c5c2f16ac3031393a4cd56e684
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2024-12-04 21:33:02 +0000

    Updated bootstrap styling

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module RecipeApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    # to allow images
+    config.assets.enabled = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
