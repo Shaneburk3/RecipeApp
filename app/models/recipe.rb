94917e05750c12d6fa3bc083ec1ee18a69bd0192
Shaneburk3 <92030458+Shaneburk3@users.noreply.github.com>
2024-12-06 22:36:13 +0000

    removed validator

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,5 @@ class Recipe < ApplicationRecord
     validates :name, presence: true, length: { maximum: 50 }
     validates :ingredients, presence: true, length: { maximum: 500 }
     validates :instructions, presence: true, length: { maximum: 1000 }
-    validates :difficulty, presence: true, inclusion: { in: [ "Easy", "easy", "Medium", "medium", "Hard", "hard" ] }, message: "Difficulty should be either Easy, Medium, or Hard.", length: { maximum: 20 }
+    validates :difficulty, presence: true, inclusion: { in: [ "Easy", "easy", "Medium", "medium", "Hard", "hard" ] }, length: { maximum: 20 }
 end
